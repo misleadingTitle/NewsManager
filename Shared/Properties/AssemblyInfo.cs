3cd91e3bbd8949ad02bd481f2cdc0648550c32a8
mminuti <mminuti3@gmail.com>
2014-11-22 17:14:06 +0100

    project implementation

--- Shared/Properties/AssemblyInfo.cs
+++ Shared/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("NewsManager")]
+[assembly: AssemblyTitle("Shared")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("HP")]
-[assembly: AssemblyProduct("NewsManager")]
+[assembly: AssemblyProduct("Shared")]
 [assembly: AssemblyCopyright("Copyright © HP 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("9a036672-6e4d-400a-82f5-fec61a465866")]
+[assembly: Guid("a7399a8f-5288-4129-a37a-eaad2bac6dec")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,7 +29,7 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Revision and Build Numbers 
+// You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
---
+++ TodoManager/INewsRESTService.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using Shared;
+
+namespace NewsManager
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IProdictRESTService" in both code and config file together.
+    [ServiceContract]
+    public interface INewsRESTService
+    {
+        [OperationContract]
+        [WebInvoke(Method="GET",
+            ResponseFormat=WebMessageFormat.Xml,
+            BodyStyle=WebMessageBodyStyle.Bare,
+            UriTemplate="GetArticleList/")]
+        List<Article> GetArticlesList();
+
+        [OperationContract]
+        [WebInvoke(Method = "GET",
+            ResponseFormat = WebMessageFormat.Xml,
+            BodyStyle = WebMessageBodyStyle.Bare,
+            UriTemplate = "GetArticleList/{Id}")]
+        Article GetArticleById(int Id);
+
+        [OperationContract]
+        [WebInvoke(Method = "PUT",
+            ResponseFormat = WebMessageFormat.Xml,
+            BodyStyle = WebMessageBodyStyle.Bare,
+            UriTemplate = "InsertArticle/{Id}")]
+        string InsertArticle(Article article);
+
+        [OperationContract]
+        [WebInvoke(Method = "PUT",
+            ResponseFormat = WebMessageFormat.Xml,
+            BodyStyle = WebMessageBodyStyle.Bare,
+            UriTemplate = "UpdateArticle/{Id}")]
+        string UpdateArticle(Article article, int Id);
+
+        [OperationContract]
+        [WebInvoke(Method = "DELETE",
+            ResponseFormat = WebMessageFormat.Xml,
+            BodyStyle = WebMessageBodyStyle.Bare,
+            UriTemplate = "DeleteArticle/{Id}")]
+        string  DeleteArticle(int Id);
+    }
+}
